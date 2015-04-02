c886c0d93687851257f65a5539ac93daeae533bf
judges119 <adam.ogrady@gmail.com>
2015-04-03 00:14:51 +0800

    Tested, now working

--- server.js
+++ server.js
@@ -33,14 +33,15 @@ Checks responses against rules and takes further action if necessary
 function requestAPI() {
   //github.authenticate(auth);
   github.events.getReceived({
-    headers: {
-      "If-None-Match": app.etag
-    },
-    user: process.argv[2];
+    headers: (typeof app.etag === 'undefined') ? undefined : { "If-None-Match": app.etag },
+    user: process.argv[2]
   }, function(err, json) {
-    (err || json.meta.status.indexOf('304 Not Modified') > -1) && return;
+    if (err || json.meta.status.indexOf('304 Not Modified') > -1) {
+      return;
+    }
     app.etag = json.meta.etag;
     delete json.meta;
+    console.log(json);
     fireHooks(matchingRules(newItems(app.recent, json)));
     app.recent = json;
   });
@@ -49,9 +50,9 @@ function requestAPI() {
 /*
 Checks the existing events against new ones from an API call, returning only new events
 */
-function newItems(initial, new) {
-  return new.reduce(function(p, c, i, a) {
-    return (c.id !== initial[0].id) ? p.concat(c) : p;
+function newItems(initial, newstuff) {
+  return newstuff.reduce(function(p, c, i, a) {
+    return (initial.length === 0 || c.id !== initial[0].id) ? p.concat(c) : p;
   }, []);
 }
 
@@ -59,8 +60,8 @@ function newItems(initial, new) {
 Checks a set of events against known rules
 Returns an array of objects, each object containing the event and any rules it triggers
 */
-function matchingRules(new) {
-  return new.reduce(function(previous, current, index, array) {
+function matchingRules(newstuff) {
+  return newstuff.reduce(function(previous, current, index, array) {
     return previous.concat({
       object: current,
       rules: app.rules.filter(function(e, i, a) {
@@ -69,8 +70,8 @@ function matchingRules(new) {
         } else {
           return (e.event.indexOf(current.type) > -1);
         }
-      });
-    }) : previous;
+      })
+    });
   }, []);
 }
 
@@ -91,7 +92,7 @@ function fireHooks(matches) {
       var req = http.request(options, function(res) {
         //Here we can check if our request was received based on the response.
       });
-      req.write(match.object);
+      req.write(JSON.stringify(match.object));
       req.end();
     });
   });
