475a7056b25229fc7f85d5ae29f2efccee032eb1
judges119 <adam.ogrady@gmail.com>
2015-04-03 00:47:29 +0800

    Added license, bugfixes

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ function requestAPI() {
     }
     app.etag = json.meta.etag;
     delete json.meta;
-    console.log(json);
     fireHooks(matchingRules(newItems(app.recent, json)));
     app.recent = json;
   });
